5c0967e5d1abaefb613b9ee31a50271fdfb0a3ce
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-11 23:39:55 +0200

    handleCellPlayed function

--- script.js
+++ script.js
@@ -11,5 +11,36 @@ const drawMessage= () => `Game ended in a draw!`
 statusDisplay.innerHTML= currentPlayerTurn()
 
 document.querySelectorAll('.cell').forEach(cell=>cell.addEventListener('click', handleCellClick))
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame)
 
+function handleCellClick(clickedCellEvent){
+    const clickedCell= clickedCellEvent.target
+    const clickedCellIndex=parseInt(
+        clickedCell.getAttribute(data-cell-index)
+    )
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive){
+        return
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex)
+    handleResultValidation();
+
+}
+
+
+function handleCellPlayed(clickedCell, clickedCellIndex){
+    gameState[clickedCellIndex]=currentPlayer
+    clickedCell.innerHTML=currentPlayer
+
+    const winningConditions=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6]
+    ]
+}
 
